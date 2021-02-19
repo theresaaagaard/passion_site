90d80e66b773481c520fc683a2df99740211c073
Christine Piilmann <christinepiilmann@christines-air.lan>
2021-02-19 13:30:11 +0100

    burgermenu tilføjet

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
      "x-apikey": "602e81e35ad3610fb5bb6352"
 
  }
+ let menuIsOpen = false;
 
  document.addEventListener("DOMContentLoaded", start);
 
@@ -12,7 +13,9 @@
  let queens;
  let filter = "alle";
 
+
  function start() {
+     document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 
      loadJSON();
  }
@@ -55,4 +58,52 @@
 
  function visDetaljer(hvem) {
      location.href = `detail.html?id=${hvem._id}`;
+ }
+
+
+ //------------------BURGER MENU-------------------
+ function toggleMenu() {
+     console.log("toggleMenu");
+
+     document.querySelector("#menuknap").classList.toggle("change");
+     document.querySelector("#navbar").classList.toggle("hidden");
+
+     // const links = document.querySelectorAll(".link");
+     // links.forEach(link => link.addEventListener("click", clickLink));
+
+     /*     function clickLink() {
+
+              if (menuIsOpen == true) {
+                  document.querySelector(".link").removeEventListener("click", clickLink);
+                  document.querySelector("#navbar").classList.add("hidden");
+
+                  document.querySelector("#navbar").classList.remove("openmenu");
+                  document.querySelector("#navbar").classList.add("closemenu");
+
+                  document.querySelector("#menuknap").classList.toggle("change");
+                  menuIsOpen = false;
+              }
+          }*/
+
+     // Undersøg om menuen er åben eller lukket
+     if (menuIsOpen == true) {
+
+         // Menuen er åben, så vi lukker den med css
+         document.querySelector("#navbar").classList.remove("openmenu");
+         document.querySelector("#navbar").classList.add("closemenu");
+
+         // menuen er nu lukket, så ændrer menuvariablen til at vise dette
+         menuIsOpen = false;
+     } else {
+
+         // menuen er lukket, så vi åbner den
+         document.querySelector("#navbar").classList.remove("closemenu");
+         document.querySelector("#navbar").classList.add("openmenu");
+
+         // Menuen er nu åben, så vi ændrer menuvariablen til at visse dette
+         menuIsOpen = true;
+
+     }
+
+     console.log(menuIsOpen);
  }
