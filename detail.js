1fa8774b931ea625200132df44d43a1b8e76265e
Theresa Aagaard Hansen <theresa_aagaard@MacBook-Pro.local>
2021-02-20 19:56:54 +0100

    singleview update

--- detail.js
+++ detail.js
@@ -23,10 +23,14 @@
         function visQueen() {
             document.querySelector(".navn").textContent += queen.navn;
             document.querySelector("#main_billede").src = medieurl + queen.billede[0];
+            document.querySelector("#ekstra_billede1").src = medieurl + queen.billede[0];
+            document.querySelector("#ekstra_billede2").src = medieurl + queen.billede[0];
+            document.querySelector("#ekstra_billede3").src = medieurl + queen.billede[0];
+            document.querySelector("#ekstra_billede4").src = medieurl + queen.billede[0];
 
             document.querySelector(".talent").textContent = queen.talent;
             document.querySelector(".placering").textContent = queen.placering;
-            document.querySelector(".varighed").textContent = queen.varighed;
+            document.querySelector(".varighed").textContent = queen.varighed + " timer";
 
             document.querySelector("#tilbage").addEventListener("click", tilbageTilOversigt);
         }
