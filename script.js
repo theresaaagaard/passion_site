7d8fe62e989cfd68498fbc628d39aa580cfd6a42
Christine Piilmann <christinepiilmann@Christines-MacBook-Air.local>
2021-02-23 11:58:37 +0100

    kommentarer i JS

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
+ //henter data fra restdb
  const medieurl = "https://dragqueens-8f96.restdb.io/media/";
  const myHeaders = {
 
-     "x-apikey": "602e81e35ad3610fb5bb6352"
+         "x-apikey": "602e81e35ad3610fb5bb6352"
 
- }
+     }
+     //opretter variabel, hvor burgermenu er lukket
  let menuIsOpen = false;
-
- document.addEventListener("DOMContentLoaded", start);
-
- console.log("DOMContentLoaded");
-
+ //opretter variabel til queens
  let queens;
+ //opretter variabel til filtering
  let filter = "alle";
 
- function start() {
+ //lytter til om DOM er loaded
+ document.addEventListener("DOMContentLoaded", start);
 
+ //når dom er loaded begynder start funktion
+ function start() {
+     console.log("DOMContentLoaded");
+
+     //toggle mellem kategori menuen er skjult eller fremme
      function myFunction(mediaquery) {
          if (mediaquery.matches) { // If media query matches
              document.querySelector(".kategorier").classList.add("hidden");
@@ -22,18 +27,19 @@
              document.querySelector(".kategorier").classList.remove("hidden");
          }
      }
-
+     //opretter variabel som lytter til skærmstørrelse
      let mediaquery = window.matchMedia("(max-width: 650px)")
      myFunction(mediaquery) // Call listener function at run time
      mediaquery.addListener(myFunction) // Attach listener function on state changes
 
+     //lytter til klik på burgermenuen
      document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 
-
+     //opretter variabel til filteringsknapper og lytter til klik på hvert enkelt og kalder funktion
      const filterKnapper = document.querySelectorAll(".kategorier button");
      filterKnapper.forEach(knap => knap.addEventListener("click", filtrerKategorier));
 
-
+     //load json data
      loadJSON();
  }
 
@@ -41,32 +47,31 @@
      filter = this.dataset.kategori;
      console.log("filtrerKategorier");
 
+     //fjerne og tilføjer valgt class på den valgte
      document.querySelector(".valgt").classList.remove("valgt");
      this.classList.add("valgt");
 
+     //kald visQueens funktion
      visQueens();
-     //     header.textContent = this.textContent;
  }
 
-
-
-
+ //load  og fetch JSON
  async function loadJSON() {
      const JSONData = await fetch("https://dragqueens-8f96.restdb.io/rest/queens", {
          headers: myHeaders
      });
      queens = await JSONData.json();
      console.log("Queens", queens);
+     //kald funktion visQueens
      visQueens();
  }
 
-
+ //tilføjer data til klon
  function visQueens() {
      console.log("visQueens");
 
      const dest = document.querySelector("#oversigt");
      const skabelon = document.querySelector("template").content;
-     dest.textContent = "";
 
      queens.forEach(queen => {
 
@@ -77,59 +82,59 @@
              klon.querySelector("img").src = medieurl + queen.billede[0];
 
 
-             klon.querySelector("#se_mere").addEventListener("click", () => visDetaljer(queen)); //laver eventlistener på ret, der fører én til en funktion, hvor man kan se detaljer om den valgte ret
+             klon.querySelector("#se_mere").addEventListener("click", () => visDetaljer(queen)); //laver eventlistener på queen, der fører én til en funktion, hvor man kan se detaljer om den valgte ret
 
+             //tilføjer klon til oversigten sektion
              dest.appendChild(klon);
 
          }
      })
  }
-
+ //gå til detaljesiden når der klikkes på den enkelte queen
  function visDetaljer(hvem) {
      location.href = `detail.html?id=${hvem._id}`;
  }
 
 
-
-
- // toggle på sortér-knappen
-
+ // toggle på sortér-knappen og kalder sorterMenu funktion
  document.querySelector(".sorter").addEventListener("click", sorterMenu);
 
-
+ //toggle class hidden på kategorier
  function sorterMenu() {
      console.log("sorterMenu");
      document.querySelector(".kategorier").classList.toggle("hidden");
  }
 
 
-
-
-
-
-
-
-
  //------------------BURGER MENU-------------------
  function toggleMenu() {
      console.log("toggleMenu");
 
+     //toggler burgermenuens udseende
      document.querySelector("#menuknap").classList.toggle("change");
+     //toggler hidden på navbar
      document.querySelector("#navbar").classList.toggle("hidden");
 
+     //opretter variabel som lytter til ethvert klik på links i navbaren og kalder clickLink funktion
      const links = document.querySelectorAll(".link");
      links.forEach(link => link.addEventListener("click", clickLink));
 
-     function clickLink() {
 
+     function clickLink() {
+         //undersøger om menuen er åben
          if (menuIsOpen == true) {
+             //luk navbaren ved click på et link inde i navbaren
              document.querySelector(".link").removeEventListener("click", clickLink);
              document.querySelector("#navbar").classList.add("hidden");
 
+             //tilføjer closemenu animation
              document.querySelector("#navbar").classList.remove("openmenu");
              document.querySelector("#navbar").classList.add("closemenu");
 
+             //ændrer burgermenu tilbage til "lukket" tilstand
              document.querySelector("#menuknap").classList.toggle("change");
+
+             //menuen er åben så ændrer variabelen til lukket
              menuIsOpen = false;
          }
      }
@@ -151,8 +156,6 @@
 
          // Menuen er nu åben, så vi ændrer menuvariablen til at visse dette
          menuIsOpen = true;
-
      }
-
      console.log(menuIsOpen);
  }
